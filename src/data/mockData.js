b0dd4fa4cf1a435b5402af0672bdf5fbc37cbbb2
Gael Arce <gaelarce3@gmail.com>
2026-02-09 17:28:08 -0600

    Diversos cambios para un MockData mas seguro se aniadio un animal mas para cumplir con requisitos de usuario final

--- src/data/mockData.js
+++ src/data/mockData.js
@@ -16,6 +16,17 @@ export const mockData = {
       allergies: ["Ninguna"],
       status: "Activo"
     },
+      {
+      id: 3,
+      name: "Pelay",
+      species: "",
+      breed: "pug",
+      age: 5,
+      weight: 28,
+      allergies: ["Ninguna"],
+      status: "Activo"
+    },
+
     {
       id: 2,
       name: "Mishi",
@@ -29,9 +40,13 @@ export const mockData = {
   ],
   vaccines: [
     { id: 1, petId: 1, name: "Rabia", date: "2025-01-15" },
-    { id: 2, petId: 1, name: "Parvovirus", date: "2024-11-20" }
+    { id: 2, petId: 1, name: "Parvovirus", date: "2024-11-20" },
+    { id: 3, petId: 3, name: "Rabia", date: "2025-01-22" }
   ],
   appointments: [
-    { id: 1, petId: 1, date: "2025-02-20", reason: "Control anual", vet: "Dr. Rodríguez" }
+    { id: 1, petId: 1, date: "2025-02-20", reason: "Control anual", vet: "Dr. Rodríguez" },
+    { id: 2, petId: 2, date: "2025-03-20", reason: "Control anual", vet: "Dr. Rodríguez" },
+    { id: 3, petId: 3, date: "2025-04-20", reason: "Control anual", vet: "Dr. Rodríguez" }
+
   ]
 };
